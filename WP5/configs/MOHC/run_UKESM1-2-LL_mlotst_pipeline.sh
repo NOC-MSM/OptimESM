65dbd2b579de0035ea4b675d652b7db3e081d821
Oliver John Tooth <otooth00@r505n017.g100.cineca.it>
2026-02-12 18:11:10 +0100

    Add MOHC WP5 NEMO Pipeline configuration files & run scripts.

---
+++ WP5/configs/MOHC/run_UKESM1-2-LL_mlotst_pipeline.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+# ================================================================
+# run_UKESSM1-2-LL_pipeline.sh
+#
+# Description: Run UKESM1-2-LL NEMO Pipeline in current process.
+#
+# Created By: Ollie Tooth (oliver.tooth@noc.ac.uk)
+# Created On: 2025-11-11   
+# ================================================================
+
+# -- Input arguments to NEMO Pipeline -- #
+# Define filepaths:
+config_file=mlotst/config_UKESM1_esm-up2p0-gwl-dn.toml
+log_file=UKESM1-2-LL_mlotst_pipeline.log
+
+# -- Python Environment -- #
+# Run this script in the env_optimesm conda virtual environment.
+
+# -- Run NEMO Pipeline CLI -- #
+# nemo_pipeline describe $config_file --log $log_file
+nemo_pipeline run $config_file --log $log_file
---
+++ WP5/configs/MOHC/run_UKESM1-2-LL_siarea_pipeline.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# ================================================================
+# run_UKESM1-2-LL_pipeline.sh
+#
+# Description: Run UKESM1-2-LL NEMO Pipeline in current process.
+#
+# Created By: Ollie Tooth (oliver.tooth@noc.ac.uk)
+# Created On: 2025-12-16
+# ================================================================
+set -euo pipefail
+
+# -- Input arguments to NEMO Pipeline -- #
+# Define filepaths:
+config_file=siarea/config_UKESM1_esm-up2p0-gwl-dn.toml
+log_file=UKESM1-2-LL_siarea_pipeline.log
+
+# Run multiple pipelines:
+l_multi=false
+
+# Define Experiment IDs [l_multi=true] -> esm-up2p0-gwl
+# exp_ids=("esm-up2p0-gwl2p0" "esm-up2p0-gwl3p0" "esm-up2p0-gwl4p0")
+
+# Define Experiment IDs [l_multi=true] -> esm-up2p0-gwl-dn
+# exp_ids=("esm-up2p0-gwl2p0-50y-dn1p0" "esm-up2p0-gwl2p0-50y-dn2p0" "esm-up2p0-gwl3p0-50y-dn2p0" "esm-up2p0-gwl4p0-50y-dn2p0" "esm-up2p0-gwl4p0-50y-dn1p0")
+
+
+# -- Python Environment -- #
+# Run this script in the env_optimesm conda virtual environment.
+
+if [ "$l_multi" = false ]; then
+    # -- Run NEMO Pipeline CLI -- #
+    # nemo_pipeline describe $config_file --log $log_file
+    nemo_pipeline run $config_file --log $log_file
+
+else
+    # Iterate over all experiment IDs:
+    for exp_id in "${exp_ids[@]}"; do
+        echo "Running ==> $exp_id"
+        # -- Updating Experiment IDs in config.toml -- #
+        sed -i "s|esm-[^/_]*|$exp_id|g" $config_file
+    
+        # -- Run NEMO Pipeline CLI -- #
+        # nemo_pipeline describe $config_file --log $log_file
+        nemo_pipeline run $config_file --log $log_file
+        echo "Completed ==> $exp_id" 
+    done
+fi
+
